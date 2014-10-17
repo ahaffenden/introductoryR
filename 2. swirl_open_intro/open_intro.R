65b1dcbd5a2d0a36a60ba81e7883d4461e29756d
ahaffenden <a.haffenden@keele.ac.uk>
2014-10-17 17:06:33 +0100

    updated slides and started 2nd session

---
+++ 2. swirl_open_intro/open_intro.R
@@ -0,0 +1,61 @@
+# This script works through the examples in the swirl package's
+# Openintro session
+#==================
+# Austin Haffenden
+# 17 Oct 14
+#==============================================================
+# In a very broad sense of the term, a scientist is one who engages 
+# in a | systematic method to acquire knowledge. More importantly, 
+# a scientist seeks to answer important questions or problems 
+# through careful observations and methods. These observations 
+# - collected from experiments, censuses, surveys, field notes, 
+# etc. - are known as DATA. Statistics is the study of how best 
+# to collect, analyze, and draw conclusions from data.
+#
+
+# Once a question has been posed, or a problem identified, the 
+# next logical step is to attempt to answer the question. 
+# Unfortunately, without any additional information, it is very 
+# difficult to answer a question, or diagnose a problem. This 
+# is where data collection comes into play! Having a set of 
+# observations, or data, related to the question will help reveal 
+# an answer.
+
+# But how exactly does one collect useful data?
+
+# Remember, when conducting research, a scientist wants to answer a question,
+# one that is usually targeted at a specific population. This population does
+# not have to be a human population, as in the most common meaning of the word.
+# Think of this population as the group of entities - people, animals, cars,
+# salaries, etc. - that is to be studied by the scientist.
+
+# Unfortunately, it is often very difficult, if not impossible, to collect
+# thorough data from an entire population. Not only can it be very
+# time-consuming to do so, it may also be an inefficient use of resources.
+# Instead, scientists target a SAMPLE population. This sample population is
+# selected in such a way that it is REPRESENTATIVE of the larger population in
+# question. In mathematical terms, this sample population is called a subset of
+# the target population.
+
+# Let's quickly test your understanding of the term 'representative.' If you
+# were interested in studying the health of men living in the U.S., ages 18-25,
+# which sample would be more representative of the target population: 
+# 1. a sample of 25 men who live in a nearby retirement home
+# 2. a sample of 25 men who are students at a local university?
+
+# 1: Men living at the retirement home
+# 2: College students
+
+# Now that you have a basic idea of how a scientist collects data, the next
+# step is to learn what to do with all of this data. While this raw data does
+# contain useful information, this information is usually specific to each
+# individual in the sample population. The scientist seeks to discover trends
+# and correlations among the subjects in the sample population, so that he or
+# she may draw conclusions about the target population as a whole.
+
+# A statistician collects data from a __________ in order to generalize results
+# to an entire __________. (Separate the two words in your response with a
+# comma.)
+
+
+
